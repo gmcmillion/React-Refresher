6c5a1d993364d2ea3ba2e902fb8f43193b6acd9c
Gregg McMillion <gmcmillion215@gmail.com>
2019-02-08 13:24:30 -0800

    Minor changes playing with jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 
 // Create a React Component
 const App = () => {
-  return <div>Hi There!</div>;
+  const buttonText = 'Click Me!';
+  const labelText = 'Enter Name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 }
 
 // Take the React Component and show on the screen
