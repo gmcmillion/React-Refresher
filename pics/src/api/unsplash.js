5d79e9cae2f13b28291202ae42f88b07161b3de3
Gregg McMillion <gmcmillion215@gmail.com>
2019-02-18 14:10:14 -0800

    Used dotenv to store unsplash client id in .env file

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID [INSERT ID]'
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_CLIENT_ID}`
   }
 });
